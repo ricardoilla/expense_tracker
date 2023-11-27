25194650a27ff8a1bc2b47857344791298840586
Ricardo Illa <ricardo.illa2@gmail.com>
2023-11-27 20:01:40 +0100

    added data validation

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -26,6 +26,34 @@ class _NewExpensesState extends State<NewExpense> {
     });
   }
 
+  //Data validation:
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      //show error message
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid input'),
+          content: const Text(
+              'Please make sure a valid title, amount, date and category was entered..'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+  }
+
   @override
   void dispose() {
     _titleController
@@ -74,7 +102,9 @@ class _NewExpensesState extends State<NewExpense> {
             )
           ],
         ),
-        const SizedBox(height: 16,),
+        const SizedBox(
+          height: 16,
+        ),
         Row(
           children: [
             DropdownButton(
@@ -106,10 +136,7 @@ class _NewExpensesState extends State<NewExpense> {
                 },
                 child: const Text('Cancel')),
             ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_amountController.text);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text('Save Expense')),
           ],
         ),
